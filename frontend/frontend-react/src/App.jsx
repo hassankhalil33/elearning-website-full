fb3f7affb2f0fbd8440d7874cefcbd3a571ff500
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-08 20:50:59 +0300

    Frontend <> Fixed Routing Issue

--- frontend/frontend-react/src/App.jsx
+++ frontend/frontend-react/src/App.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
 import Admin from "./components/Admin";
 
 function App() {
     return (
-    <Routes>
-        <Route path="/" element={
-            <section className="container">
-                <LoginForm />
-            </section>
-        } />
+    <Router>
+        <Routes>
+            <Route path="/" element={
+                <section className="container">
+                    <LoginForm />
+                </section>
+            } />
 
-        <Route path="/admin" element={
-            <section className="container">
-                <Admin />
-            </section>
-        } />
-        
-        {/* <Route path="/client" element={<Client />} />
-        <Route path="/teacher" element={<Teacher />} /> */}
-    </Routes>
+            <Route path="/admin" element={
+                <section className="container">
+                    <Admin />
+                </section>
+            } />
+
+            {/* <Route path="/client" element={<Client />} />
+            <Route path="/teacher" element={<Teacher />} /> */}
+        </Routes>
+    </Router>
     )
 }
 
