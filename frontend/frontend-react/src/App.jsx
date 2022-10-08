2d1bd1f43dbb8021c524265f3c0ae7ced5622035
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-08 19:39:04 +0300

    Frontend <> Added Routes, Created Admin Component

--- frontend/frontend-react/src/App.jsx
+++ frontend/frontend-react/src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
+import Admin from "./components/Admin";
 
 function App() {
     return (
-    <section className="login-section">
-        <LoginForm />
-    </section>
+    <BrowserRouter>
+        <section className="login-section">
+            <LoginForm />
+        </section>
+        <Routes>
+            <Route path="/admin" element={<Admin />} />
+            {/* <Route path="/client" element={<Client />} />
+            <Route path="/teacher" element={<Teacher />} /> */}
+        </Routes>
+    </BrowserRouter>
     )
 }
 
