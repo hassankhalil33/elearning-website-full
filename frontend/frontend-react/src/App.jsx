8c093119746df2ec4fb4b81e508ac0dd46e081cd
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-08 17:30:10 +0300

    Frontend <> Divided App to LoginForm Component, Imported

--- frontend/frontend-react/src/App.jsx
+++ frontend/frontend-react/src/App.jsx
@@ -1,33 +1,10 @@
 import React from "react";
-import Input from "./components/Input";
-import Button from "./components/Button";
+import LoginForm from "./components/LoginForm";
 
 function App() {
     return (
     <section className="login-section">
-        <form className="login-form">
-            <h3>E-Tuter</h3>
-            <div>
-                <Input
-                    type={"text"}
-                    name={"username"}
-                    placeholder={"Username"}
-                />
-                <Input
-                    type={"password"}
-                    name={"password"}
-                    placeholder={"Password"}
-                />
-                <Input
-                    type={"password"}
-                    name={"password-confirm"}
-                    placeholder={"Confirm Password"}
-                />
-            </div>
-            <Button
-                content={"Sign In"}
-            />
-        </form>
+        <LoginForm />
     </section>
     )
 }
---
+++ frontend/frontend-react/src/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+import Input from "./Input";
+import Button from "./Button";
+
+function LoginForm() {
+    return (
+        <form className="login-form">
+            <h3>E-Tuter</h3>
+            <div>
+                <Input
+                    type={"text"}
+                    name={"username"}
+                    placeholder={"Username"}
+                />
+                <Input
+                    type={"password"}
+                    name={"password"}
+                    placeholder={"Password"}
+                />
+                <Input
+                    type={"password"}
+                    name={"password-confirm"}
+                    placeholder={"Confirm Password"}
+                />
+            </div>
+            <Button
+                content={"Sign In"}
+            />
+        </form>
+    )
+}
+
+export default LoginForm;
