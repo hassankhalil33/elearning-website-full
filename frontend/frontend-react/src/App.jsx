7ac74bce1c70c754173eb29a72fe035f12da107a
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-09 12:13:01 +0300

    Frontend <> Fixed Styling Issue with Login Page

--- frontend/frontend-react/src/App.jsx
+++ frontend/frontend-react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
         <Routes>
             <Route path="/" element={
-                <section className="container">
+                <section className="login-section">
                     <LoginForm />
                 </section>
             } />
