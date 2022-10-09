9d87cf24d50c7bc8d4e6beb0b031e62d005feeee
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-10 00:47:26 +0300

    Frontend <> Stylized Table Content in Admin Screens

--- frontend/frontend-react/src/screens/Admin/AdminStudents.jsx
+++ frontend/frontend-react/src/screens/Admin/AdminStudents.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Admin from "../../components/Admin";
 import Button from "../../components/Button";
+import defaultPic from "../../assets/images/default_pic.png";
 
 function AdminStudents() {
 
@@ -18,11 +19,26 @@ function AdminStudents() {
             />
           </div>
           <div>
-            <div>
+            <section className="table-content">
               <p>ID</p>
               <p>Photo</p>
               <p>Name</p>
               <p>Course</p>
+              <p>Edit</p>
+            </section>
+            <div>
+              <p>1</p>
+              <p><img src={defaultPic} alt="" /></p>
+              <p>Charbel Maroun</p>
+              <p>FSWO5</p>
+              <p></p>
+            </div>
+            <div>
+              <p>2</p>
+              <p><img src={defaultPic} alt="" /></p>
+              <p>Jad Yahya</p>
+              <p>FSWO5</p>
+              <p></p>
             </div>
           </div>
         </section>
