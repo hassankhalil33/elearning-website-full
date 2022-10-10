bbffe0ca5c2a91e1359c36a93bce529cfec59d3f
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-10 15:29:51 +0300

    Frontend <> Added Student Dashboard Screen

---
+++ frontend/frontend-react/src/screens/Student/StudentDashboard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Student from "../../components/Student";
+
+function StudentDashboard() {
+
+  return (
+    <Student>
+      <div className="admin-feed teacher-feed">
+        <div>
+          <h1>Teacher Dashboard</h1>
+        </div>
+        <section className="admin-lower-feed">
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </section>
+      </div>
+    </Student>
+  )
+}
+
+export default StudentDashboard;
