a6b3102ff542f0a22af34bbd81ea2348385c40f3
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-11 21:55:05 +0300

    Backend <> Created Api Controller, Added Routes

---
+++ backend/laravel-backend/app/Http/Controllers/ApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller {
+    
+    public function students() {
+
+    }
+
+    public function teachers() {
+        
+    }
+
+    public function announcements() {
+        
+    }
+
+    public function courses() {
+        
+    }
+}
--- backend/laravel-backend/routes/api.php
+++ backend/laravel-backend/routes/api.php
@@ -3,17 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post("/students", [ApiController::class, "students"])->name("students");
+Route::post("/teachers", [ApiController::class, "teachers"])->name("teachers");
+Route::post("/announcements", [ApiController::class, "announcements"])->name("announcements");
+Route::post("/courses", [ApiController::class, "courses"])->name("courses");
