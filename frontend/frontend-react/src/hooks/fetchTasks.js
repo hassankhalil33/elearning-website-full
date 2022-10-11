6d21b0b39fc91baf4932b28b36f393c7bef115f1
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-11 23:06:36 +0300

    Linking <> Weird Error in Fetch Data Axios Hook

--- frontend/frontend-react/src/hooks/fetchTasks.js
+++ frontend/frontend-react/src/hooks/fetchTasks.js
@@ -1,28 +1,28 @@
-import axios from "axios";
-
-const useFetchData = async () => await axios("https://dog.ceo/api/breeds/image/random");
-
-export default useFetchData
-
-//WORKING:
-// import { useEffect, useState } from "react";
 // import axios from "axios";
 
-// const useFetchData = () => {
-//   const [data, setData] = useState({});
+// const useFetchData = async () => await axios("https://dog.ceo/api/breeds/image/random");
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const { data: response } = await axios.get("https://dog.ceo/api/breeds/image/random");
-//       setData(response);
-//     };
+// export default useFetchData
 
-//     fetchData();
-//   }, []);
+//WORKING:
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-//   return {
-//     data,
-//   };
-// };
+const fetchData = (api_url) => {
+  const [data, setData] = useState({});
 
-// export default useFetchData;
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data: response } = await axios.get(`http://127.0.0.1:8000/api/${api_url}`);
+      setData(response);
+    };
+
+    fetchData();
+  }, []);
+
+  return {
+    data,
+  };
+};
+
+export default fetchData;
