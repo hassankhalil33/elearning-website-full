af81b96900c9e579ca53b3746e62fe57b721ecfe
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-11 16:29:33 +0300

    Backend <> Composer wont Read dll Extension

--- backend/laravel-backend/config/database.php
+++ backend/laravel-backend/config/database.php
@@ -77,6 +77,16 @@ return [
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
+        
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_DB_HOST', 'localhost'),
+            'port'     => env('MONGO_DB_PORT', 27017),
+            'database' => env('MONGO_DB_DATABASE'),
+            'username' => env('MONGO_DB_USERNAME'),
+            'password' => env('MONGO_DB_PASSWORD'),
+            'options'  => []
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
