b0fd1c205b0a631aca6e4a0c752a38b2cb8123e2
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-10-11 22:07:45 +0300

    Backend <> Fixed Issue Where Package was not Installed Properly

--- backend/laravel-backend/app/Models/User.php
+++ backend/laravel-backend/app/Models/User.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+// // use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Notifications\Notifiable;
+// use Laravel\Sanctum\HasApiTokens;
+
+// class User extends Authenticatable {
+//     // use HasApiTokens, HasFactory, Notifiable;
+// }
+
+use Jenssegers\Mongodb\Auth\User as Authenticatable;
 
 class User extends Authenticatable {
-    // use HasApiTokens, HasFactory, Notifiable;
+
 }
